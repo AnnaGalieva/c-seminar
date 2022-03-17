4d6e395cee34da1abfbf2a1946b94e293c0922c6
Anna <anutasash@mail.ru>
2022-03-17 11:03:18 +0300

    dz10

---
+++ z71/Program.cs
@@ -0,0 +1,40 @@
+﻿// 
+
+/*int[] array = { 1, 2, 3, 4, 5 };
+double sum = 0, result = 0;
+for (int i = 0; i < array.length; i++)
+{
+    sum += array[i];
+}
+result = sum / array.Length;
+
+Console.WriteLine("среднее арифметическое: " + result);*/
+
+
+/*double sum = 0, avg = 0;
+double[] numbers = { 10, 20, 50, 40 };
+for (int i = 0; i < numbers.Length; i++)
+{
+    sum += numbers[i];
+}
+avg = sum / numbers.Length;
+//Console.WriteLine("The Sum is : " + sum);
+Console.WriteLine("The Average is : " + avg);*/
+
+/*int[] mas = new int[10];
+Random r = new Random();
+int summ = 0;
+Console.WriteLine("Заполнение массива случайными числами:");
+for (int i = 0; i < 10; i++)
+{
+    mas[i] = r.Next(10);
+    Console.Write(mas[i] + " ");
+}
+Console.WriteLine("\n\nСреднее арифметическое элементов:");
+for (int i = 0; i < mas.Length; i++)
+    summ += mas[i];
+int sr = summ / mas.Length;
+Console.WriteLine(sr);
+Console.Read();*/
+
+
---
+++ zadacha71/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 73 : Есть число N. Сколько групп M, можно получить при разбиении всех 
+//чисел на группы, так чтобы в одной группе все числа были взаимно просты 
+//(все числа в группе друг на друга не делятся)? Найдите M при заданном N и 
+//получите одно из разбиений на группы N ≤ 10²⁰.
+//Например, для N = 50, M получается 6
+
+/*Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine());
+int m = (int)Math.Log2(n)+1;
+Console.WriteLine($"числа до {n} разбиваются на {m} групп");
+Console.WriteLine($"Группа 1:   1.");
+
+for (int i = 1; i < m-1; i++)
+{
+        Console.WriteLine($"Группа {i+1}: от {Math.Pow(2, i)} до {Math.Pow(2, i+1)-1}");
+    }
+Console.WriteLine($"Группа {m}: от {Math.Pow(2, m-1)} до {n}" );*/
+
+Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine());
+int group = 0;
+for (int i = 1; i < n; i++)
+{
+    if (i == Math.Pow(2, group))
+    {
+        Console.WriteLine();
+        group++;
+        Console.Write($"Группа {group}: ");
+    }
+    Console.Write($"{i} ");
+}
+Console.WriteLine();
+Console.WriteLine();
